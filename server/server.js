453b0e9c6bc8839d13b28fe7b969a4410bd137e6
David Fox <dafox1@my.waketech.edu>
2021-06-01 20:41:35 -0400

    changed password

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 
 // Connection URL
 mongoose.Promise = global.Promise
-const uri = "mongodb+srv://davidfox:M7gpjycf@cluster0.ilxnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const uri = "mongodb+srv://davidfox:MERNcluster@cluster0.ilxnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.connection.on('error', () => {
   throw new Error('unable to connect to database: ${mongoUri}')
